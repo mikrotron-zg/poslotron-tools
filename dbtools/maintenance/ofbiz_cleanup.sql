547dea5044f1d6dab5c73c60f61d12f3abca1d5a
Tomislav Preksavec <tomislav.preksavec@gmail.com>
2024-07-01 17:49:53 +0200

    Make cleanup cycles shorter

--- dbtools/maintenance/ofbiz_cleanup.sql
+++ dbtools/maintenance/ofbiz_cleanup.sql
@@ -1,7 +1,7 @@
-delete from party_data_source where date(created_stamp) < date(current_timestamp - '90 days'::interval);
-delete from product_search_constraint where date(created_stamp) < date(current_timestamp - '90 days'::interval);
-delete from product_search_result where date(created_stamp) < date(current_timestamp - '90 days'::interval);
-delete from cart_abandoned_line where date(created_stamp) < date(current_timestamp - '90 days'::interval);
-delete from visit where date(created_stamp) < date(current_timestamp - '90 days'::interval);
+delete from party_data_source where date(created_stamp) < date(current_timestamp - '40 days'::interval);
+delete from product_search_constraint where date(created_stamp) < date(current_timestamp - '40 days'::interval);
+delete from product_search_result where date(created_stamp) < date(current_timestamp - '40 days'::interval);
+delete from cart_abandoned_line where date(created_stamp) < date(current_timestamp - '40 days'::interval);
+delete from visit where date(created_stamp) < date(current_timestamp - '41 days'::interval);
 -- last one only works after all the above have executed
-delete from visitor vtr where date(created_stamp) < date(current_timestamp - '91 days'::interval) and not exists (select from visit vt where vt.visitor_id = vtr.visitor_id);
+delete from visitor vtr where date(created_stamp) < date(current_timestamp - '42 days'::interval) and not exists (select from visit vt where vt.visitor_id = vtr.visitor_id);
